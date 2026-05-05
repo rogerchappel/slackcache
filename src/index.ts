318669f8aa8ab046d7df2d038c86730b4ec42120
Roger Chappel <miscanalysis@gmail.com>
2026-05-05 18:36:10 +1000

    feat: expose library API

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { buildIndex } from './indexer.js';
+export { searchIndex, threadMessages } from './search.js';
+export { loadIndex, saveIndex } from './store.js';
+export type { CachedMessage, ScopeReport, SearchHit, SlackCacheIndex, SlackChannel, SlackMessage, SlackUser } from './types.js';
